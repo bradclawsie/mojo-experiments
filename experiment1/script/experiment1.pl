756272618b88bcb5634c4975dbba06c63b891c10
Brad Clawsie <brad@b7j0c.org>
2025-10-30 09:43:38 -0500

    add experiment1 with helper and stash

---
+++ experiment1/script/experiment1.pl
@@ -0,0 +1,10 @@
+#!/usr/bin/env perl
+
+use v5.42;
+use Mojo::File qw(curfile);
+use lib curfile->dirname->sibling('lib')->to_string;
+use Mojolicious::Commands;
+
+Mojolicious::Commands->start_app('experiment1');
+
+__END__
